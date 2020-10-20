b8c019fb692142bd2fea1d347db9a7c3342fecb7
nparsley <nickparsley5@gmail.com>
2020-10-19 20:39:45 -0700

    add shopping list service + ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg'),
-        new Recipe('Another test recipe', 'This is simply a test', 'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg') 
+        new Recipe(
+            'A test recipe',
+            'This is simply a test', 
+            'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg',
+            [
+                new Ingredient('Pepporoni', 8),
+                new Ingredient('Mozzarella', 10)
+            ]),
+        new Recipe(
+            'Another test recipe', 
+            'This is simply a test', 
+            'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg',
+            [
+                new Ingredient('Sausage', 8),
+                new Ingredient('Cheddar', 10)
+            ]) 
       ];
 
       getRecipes() {
