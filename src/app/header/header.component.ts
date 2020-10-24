16cefa60c5a60743d51ebeab88ed4796dbe6a252
nparsley <nickparsley5@gmail.com>
2020-10-24 00:56:57 -0700

    routes setup-add nav, active routes, reload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 //import { EventEmitter } from 'protractor';
 
 
@@ -7,12 +7,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>();
 
-    collapsed = true;
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
 }
 
