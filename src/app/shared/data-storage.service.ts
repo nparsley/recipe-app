997a59a3638ca9edc7e4c00a005f0449449293d9
nparsley <nickparsley5@gmail.com>
2020-11-02 21:21:21 -0700

    resolving data before loading + resolver edit

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -22,15 +22,19 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
         .get<Recipe[]>('https://ng-course-recipe-book-27f42.firebaseio.com/recipes.json')
-        .pipe(map(recipes => {
+        .pipe(
+            map(recipes => {
             return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+                };
             });
-        }))
-        .subscribe(recipes => {
+        }),
+        tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        });
+        })
+        )
+
     }
 }
