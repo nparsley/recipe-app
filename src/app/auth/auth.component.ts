9e36e97e03161bf5eb4d046733b9615c4525bbe9
nparsley <nickparsley5@gmail.com>
2020-11-03 14:44:26 -0700

    signup request-sending, spinner, error handle logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 
 @Component({
@@ -8,13 +9,39 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+
+    constructor(private authService: AuthService) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        this.isLoading = true;
+        if (this.isLoginMode) {
+            //...
+        } else {
+            this.authService.signup(email, password).subscribe(
+                resData => {
+                    console.log(resData);
+                    this.isLoading = false;
+                },
+                errorMessage => {
+                    console.log(errorMessage);
+                    this.error = errorMessage;
+                    this.isLoading = false;
+                }
+            );
+        }
+
         form.reset();
     }
 }
