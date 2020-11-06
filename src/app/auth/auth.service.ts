d49a841a9e957fef0359d36457c856713eb9ab03
nparsley <nickparsley5@gmail.com>
2020-11-05 23:59:28 -0700

    auto-login + logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { catchError, tap } from 'rxjs/operators';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 
 export interface AuthResponseData {
@@ -19,7 +20,7 @@ export class AuthService {
     user = new BehaviorSubject<User>(null);
     
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private router: Router) {
 
     }
 
@@ -65,6 +66,32 @@ export class AuthService {
         );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(
+            userData.email, 
+            userData.id, 
+            userData._token, 
+            new Date(userData._tokenExpirationDate)
+        );
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
+    }
+
     private handleAuthentication(
         email: string, 
         userId: string, 
@@ -81,6 +108,7 @@ export class AuthService {
             expirationDate
             );
             this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
