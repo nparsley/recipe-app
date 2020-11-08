07370ec1a92dc1871599985e22d020c4cfdf71a7
nparsley <nickparsley5@gmail.com>
2020-11-08 01:20:18 -0700

    recipe,shopping-list,auth-lazy loading + module preload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
-import { AuthComponent } from './auth/auth.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
@@ -22,11 +18,8 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
-    CoreModule,
-    AuthModule
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -15,7 +14,7 @@ import { ShoppingListComponent } from './shopping-list.component';
     imports: [
         FormsModule,
         RouterModule.forChild([
-            { path: 'shopping-list', component: ShoppingListComponent }
+            { path: '', component: ShoppingListComponent }
         ]),
         SharedModule
     ]
