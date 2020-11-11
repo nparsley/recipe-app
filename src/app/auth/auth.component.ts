40de716bcff4e09b6771162244ec5879eccee04b
nparsley <nickparsley5@gmail.com>
2020-11-10 20:35:56 -0700

    auth-effects,error handling, login (NgRx)

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,6 +5,9 @@ import { Observable, Subscription } from 'rxjs';
 import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/shared/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 
 @Component({
@@ -19,7 +22,12 @@ export class AuthComponent implements OnDestroy {
 
     private closeSub: Subscription;
 
-    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
+    constructor(
+        private authService: AuthService, 
+        private router: Router, 
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private store: Store<fromApp.AppState>
+        ) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -37,7 +45,8 @@ export class AuthComponent implements OnDestroy {
         this.isLoading = true;
 
         if (this.isLoginMode) {
-            authObs = this.authService.login(email, password);
+            // authObs = this.authService.login(email, password);
+            this.store.dispatch(new AuthActions.LoginStart({ email: email, password: password }));
         } else {
             authObs = this.authService.signup(email, password);
         }
