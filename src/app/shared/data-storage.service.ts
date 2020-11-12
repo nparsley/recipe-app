6af71e4abc7bdd470e8b314ddbcf3deedc8256f9
nparsley <nickparsley5@gmail.com>
2020-11-12 14:27:53 -0700

    recipes-NgRx setup

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+    constructor(
+        private http: HttpClient, 
+        private recipeService: RecipeService, 
+        private store: Store<fromApp.AppState>
+        ) {}
 
 
     storeRecipes() {
@@ -38,7 +44,8 @@ export class DataStorageService {
                 });
             }),
             tap(recipes => {
-                this.recipeService.setRecipes(recipes);
+                // this.recipeService.setRecipes(recipes);
+                this.store.dispatch(new RecipesActions.SetRecipes(recipes));
             })
         );
     }
